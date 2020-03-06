48262a432e680ac1a9f60c529e8873d03791ba4a
nomizo-iox <nomizo.iox@gmail.com>
2020-03-05 18:36:48 -0600

    finished making the contact model.

---
+++ src/main/java/com/nomizo/Contact.java
@@ -0,0 +1,55 @@
+package com.nomizo;
+
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
+import java.util.Objects;
+
+public class Contact {
+
+    private Integer id;
+    private String name;
+    private String email;
+    private String address;
+    private String phone;
+
+    public Contact() { super(); }
+
+    public Contact(Integer id, String name, String email, String address, String phone) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.address = address;
+        this.phone = phone;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Contact contact = (Contact) obj;
+        return id.equals(contact.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    public Integer getId() { return id; }
+    public void setId(Integer id) { this.id = id; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public String getAddress() { return address; }
+    public void setAddress(String address) { this.address = address; }
+
+    public String getPhone() { return phone; }
+    public void setPhone(String phone) { this.phone = phone; }
+
+    @Override
+    public String toString() { return ToStringBuilder.reflectionToString(this); }
+}
